84e86a6f286894f148e493dd6754b8d48d156cc8
Gösta Ostendorf <goesta.o@gmail.com>
2017-01-12 22:13:30 +0100

    db

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Configuration(object):
-    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' %APPLICATION_DIR
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:cj4h8ofm@localhost:5432/blog'
+
+
--- app/models.py
+++ app/models.py
@@ -5,14 +5,22 @@ from app import db
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
 
+entry_tags = db.Table('entry_tags', db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')), db.Column('entry_id', db.Integer, db.ForeignKey('entry.id')))
+
 
 class Entry(db.Model):
+    STATUS_PUBLIC = 0
+    STATUS_DRAFT = 1
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     slug = db.Column(db.String(100), unique=True)
     body = db.Column(db.Text)
+    status = db.Column(db.SmallInteger, default = STATUS_PUBLIC)
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    tags = db.relationship('Tag', secondary=entry_tags,
+                           backref=db.backref('entries', lazy='dynamic'))
 
     def _init__(self, *args, **kwargs):
         super(Entry.self).__init__(*args, **kwargs)  # Call parent constructor
@@ -23,5 +31,20 @@ class Entry(db.Model):
         if self.title:
             self.slug = slugify(self.title)
 
+    # String representation for object of type Entry
     def __repr__(self):
         return '<Entry: %s>' % self.title
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+
+    def __repr__(self):
+        return '<Tag %s>' % self.name
